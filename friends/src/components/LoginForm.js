85a19b84be82c2e60cd288db31ea63cae52c38c1
Dan Sample <danssample@gmail.com>
2019-06-27 17:51:36 -0700

    Built out LOGIN_START case in reducer

--- friends/src/components/LoginForm.js
+++ friends/src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { login } from '../actions';
 
 class LoginForm extends Component {
   state = {
--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,7 +1,22 @@
-import { combineReducers } from 'redux';
+import { LOGIN_START } from '../actions';
 
 const initialState = {
-  friends: []
+  friends: [],
+  inLoggingIn: false,
+  isLoading: false,
+  isFetchingData: false,
+  error: ''
 };
 
-export default combineReducers({});
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_START: {
+      return {
+        ...state,
+        isLoggingIn: true
+      };
+    }
+  }
+};
+
+export default reducer;
