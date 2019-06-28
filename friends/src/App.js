57e48ee930bd3a3c03da9043762b8a48e0e2ff24
Dan Sample <danssample@gmail.com>
2019-06-27 23:02:08 -0700

    some files that werent saved from last commit

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
         <Route path="/login" component={LoginForm} />
+        <PrivateRoute exact path="/protected" />
       </div>
     </Router>
   );
